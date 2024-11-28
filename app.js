266491532d00ef4587c8db3914cf359e118907c3
k8vlar <kevin.vellard.gmail.com>
2024-11-28 16:39:15 +0100

    Mise à jour des URL API et configuration CORS

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const sequelize = require("./database");
-const setupSwagger = require("./config/swagger"); // Importer la configuration Swagger
+const setupSwagger = require("./config/swagger"); Swagger
 const path = require("path");
 const jokeRoutes = require("./routes/v1/jokes");
 
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://k8vlar.github.io/carambarfrontend' 
+}));
 app.use(express.json());
 
 // Configurer Swagger
 setupSwagger(app);
 
 // Routes API
-app.use("/jokes", jokeRoutes); // Utiliser le routeur pour les routes jokes
+app.use("/jokes", jokeRoutes); 
 
 // Démarrage du serveur et synchronisation de la base de données
 sequelize.sync().then(() => {
